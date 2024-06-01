dd422b57feac9c22e9e451ae96d70da7ba598188
aristeu-garcia <aristeu.inbox@gmail.com>
2024-06-01 12:43:08 -0300

    feat: test

--- application/tests/routes/test_check.py
+++ application/tests/routes/test_check.py
@@ -1,4 +1,4 @@
-def test_home_page(client):
+def test_health_check(client):
     response = client.get('/api/v1/check/')
     assert response.status_code == 200
     assert b'{"message":"> Api is alive! <"}\n' in response.data
