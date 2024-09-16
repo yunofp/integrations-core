f2c74be0598b0943851d053ee9d90cdb8ffddf65
aristeu-garcia <aristeu.inbox@gmail.com>
2024-09-16 12:22:32 -0300

    feat: finalizando indicacoes

--- application/blueprints/services/performance_service.py
+++ application/blueprints/services/performance_service.py
@@ -8,6 +8,10 @@ class PerformanceService:
 
     def calculate_indications_type_by_year_group_by_month(self, year):
         indications = self.indications_repository.find_many_by_year(year)
+
+        if not indications:
+            return {"message": "No indications found for this year"}
+
         indications_dataframe = pandas.DataFrame(indications)
 
         months_list = date_utils.get_months_list()
@@ -19,10 +23,33 @@ class PerformanceService:
             indications_dataframe["month"], categories=months_list, ordered=True
         )
 
-        indications_by_month = (
-            indications_dataframe.groupby("month", observed=False)["inclusion_date"]
-            .count()
-            .reindex(months_list, fill_value=0)
-            .to_dict()
+        indications_dataframe["is_lead"] = (
+            (indications_dataframe["status"] == "ANDAMENTO")
+            & (indications_dataframe["closing_date"].isna())
+            & (indications_dataframe["implantation_date"].isna())
         )
-        print(indications_by_month)
+
+        indications_dataframe["is_rd"] = (
+            indications_dataframe["rd_date"].notna()
+            & (indications_dataframe["status"] == "ANDAMENTO")
+            & (indications_dataframe["closing_date"].isna())
+            & (indications_dataframe["implantation_date"].isna())
+        )
+
+        indications_dataframe["is_client"] = (
+            indications_dataframe["closing_date"].notna()
+            & indications_dataframe["implantation_date"].notna()
+        )
+
+        list = ["is_lead", "is_rd", "is_client"]
+        indications_processed = {}
+        for i in list:
+            indications_by_month = (
+                indications_dataframe.groupby("month", observed=False)[i]
+                .sum()
+                .reindex(months_list, fill_value=0)
+                .to_dict()
+            )
+            indications_processed[i] = indications_by_month
+
+        print(indications_processed)
